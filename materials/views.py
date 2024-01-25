d907ae1d2d64c05fe3bff83bc995923a3a8a5303
bargest <DIZz92@mail.ru>
2024-01-25 17:50:16 +0300

    add 21.1/5.1

--- materials/views.py
+++ materials/views.py
@@ -1,14 +1,23 @@
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, ListView, DetailView, DeleteView
 
 from materials.models import Material
 
 
 class MaterialCreateView(CreateView):
     model = Material
-    fields = ('title', 'body', )
+    fields = ('title', 'body',)
     success_url = reverse_lazy('materials:list')
 
 
 class MaterialListView(ListView):
     model = Material
+
+
+class MaterialDetailView(DetailView):
+    model = Material
+
+
+class MaterialDeleteView(DeleteView):
+    model = Material
+    success_url = reverse_lazy('material:list')
