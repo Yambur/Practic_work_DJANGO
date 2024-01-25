f46111764edc047b06b97e81f295a58f006194a1
bargest <DIZz92@mail.ru>
2024-01-25 22:30:11 +0300

    add 21.1/6

--- materials/urls.py
+++ materials/urls.py
@@ -8,6 +8,6 @@ app_name = MaterialsConfig.name
 urlpatterns = [
     path('create/', MaterialCreateView.as_view(), name='create'),
     path('', MaterialListView.as_view(), name='list'),
-    #path('edit/<int:pk>/', MaterialDetailView.as_view(), name='edit'),
+    path('edit/<int:pk>/', MaterialDetailView.as_view(), name='edit'),
     #path('delete/<int:pk>/', ..., name='delete'),x`
 ]
